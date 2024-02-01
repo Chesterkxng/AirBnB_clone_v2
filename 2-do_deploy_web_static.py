a3c06715d15d6d78d3bde5362d4c3bd0fa108c54
Chesterkxng <cgoita00@gmail.com>
2024-02-01 21:23:39 +0000

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,57 +2,40 @@
 """
 TGZ
 """
-from datetime import datetime
 from fabric.api import *
 import os
-from os.path import exists
 
 env.hosts = ['100.25.203.135', '100.25.46.169']
 
 
-def do_pack():
-    """
-    Return TGZ or none
-    """
-    now = datetime.now()
-    time_string = now.strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_{}.tgz".format(time_string)
-    local("sudo mkdir -p versions")
-    archive_path = "versions/{}".format(archive_name)
-    command = "sudo tar -czvf {} web_static".format(archive_path)
-    result = local(command)
-    if result.failed:
-        return None
-    return archive_path
-
-
 def do_deploy(archive_path):
-    if not exists(archive_path):
-        return False
-
+    """distributing archives to the web servers"""
+    if not os.path.exists(archive_path):
+	return False
     try:
-        put(archive_path, '/tmp/')
-
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-
-        run('sudo mkdir -p /data/web_static/releases/{}/'.format(foldername))
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(filename, foldername))
-
-        run('sudo rm /tmp/{}'.format(filename))
-
-        run('sudo mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.
-            format(foldername, foldername))
-
-        run('sudo rm -rf /data/web_static/current')
-
-        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.
-            format(foldername))
-
-        print('New version deployed!')
-        return True
-
-    except:
-        return False
+	file_name = archive_path.split("/")[-1]
+	no_extension = file_name.split(".")[0]
+	release_path = "/data/web_static/releases/"
+	# Upload the archive to /tmp/
+	put(archive_path, '/tmp/')
+	# Create the release directory
+	run('sudo mkdir -p {}{}/'.format(release_path, no_extension))
+	# Extract the contents to the release directory
+	run('sudo tar -xzf /tmp/{} -C {}{}/'.format(
+	    file_name, release_path, no_extension))
+	# Remove the temporary archive file
+	run('sudo rm /tmp/{}'.format(file_name))
+	# Move contents from web_static subdirectory to release directory
+	run('sudo mv {}/web_static/* {}/'.format(
+	    release_path + no_extension, release_path + no_extension))
+	# Remove the web_static directory within the release directory
+	run('sudo rm -rf {}/web_static'.format(release_path + no_extension))
+	# Remove the current symbolic link
+	run('sudo rm -rf /data/web_static/current')
+	# Create a new symbolic link
+	run('sudo ln -s {}/ /data/web_static/current'.format(
+	    release_path + no_extension))
+	return True
+    except Exception as e:
+	print("Error: {}".format(str(e)))
+	return False
