93df6b7e83a81cce411bfd45dd6b41a1b08ab37a
Chesterkxng <cgoita00@gmail.com>
2024-02-16 10:33:47 +0000

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application:
+listening on 0.0.0.0, port 5000
+Routes: /: display “Hello HBNB!”
+
+"""
+from markupsafe import escape
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Display HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Display C is ...
+    """
+    return f'C {escape(text).replace("_", " ")}'
+
+
+@app.route("/python/", defaults={'text': "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """
+    Display Python is ...
+    """
+    return f'Python {escape(text).replace("_", " ")}'
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
