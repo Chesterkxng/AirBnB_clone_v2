867257e88c6db117eb0a49c269ac7ed1b4909b6e
Chesterkxng <cgoita00@gmail.com>
2024-02-16 12:31:26 +0000

    alchemy

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,7 +53,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -62,3 +62,9 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """
+        method for deserializing the JSON file to objects
+        """
+        self.reload()
--- models/place.py
+++ models/place.py
@@ -1,18 +1,60 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from sqlalchemy import Table, Column, String, ForeignKey
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    amenities = relationship(
+        "Amenity",
+        secondary="place_amenity",
+        backref="places",
+        viewonly=False
+    )
+
+    @property
+    def amenities(self):
+        """ Getter attribute for FileStorage """
+        from models import storage
+        amenities_list = []
+        for amenity_id in self.amenity_ids:
+            amenity = storage.get("Amenity", amenity_id)
+            if amenity:
+                amenities_list.append(amenity)
+        return amenities_list
+
+    @amenities.setter
+    def amenities(self, obj):
+        """ Setter attribute for FileStorage """
+        if isinstance(obj, Amenity):
+            if not hasattr(self, 'amenity_ids'):
+                setattr(self, 'amenity_ids', [])
+            if obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
+
+
+metadata = Base.metadata
+
+place_amenity = Table(
+    'place_amenity', metadata,
+    Column("place_id", String(60), ForeignKey("places.id"),
+           primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"),
+           primary_key=True, nullable=False)
+)
